b71aa04f3a23a0825f81a448c41285666af9d141
cganzon <crganzon@gmail.com>
2022-10-22 00:08:28 -0700

    create App.css and add some css resets

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
 import UserData from "./components/UserData";
+import "./App.css";
 
 export default function App() {
     return (
