251fa0677399d1c4be34a4e80df170cc596315a1
cganzon <crganzon@gmail.com>
2022-10-22 18:09:21 -0700

    create a Header folder within the components folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Header from "./components/Header";
+import Header from "./components/Header/Header";
 import SearchBar from "./components/SearchBar";
 import UserData from "./components/UserData";
 import "./App.css";
