794f8b1c350db6621f3aabb23ec14f3f6bcaa6aa
cganzon <crganzon@gmail.com>
2022-10-23 18:16:18 -0700

    change user state object to be initialized as an empty string

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,24 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./SearchBar.css";
 
 export default function SearchBar() {
-    const [ user, setUser ] = useState({
-        username: ""
-    });
-
-    console.log(user.username)
+    const [ username, setUsername ] = useState("");
     
     function handleChange(e) {
-        const { name, value } = e.target;
-        setUser(prevUser => ({
-            ...prevUser,
-            [name]: value
-        }));
+        setUsername(e.target.value);
     };
 
     return (
         <form className="search-form">
-            <input type="text" name="username" value={user.username} onChange={handleChange}/>
+            <input type="text" name="username" value={username} onChange={handleChange}/>
             <button>Search</button>
         </form>
     );
