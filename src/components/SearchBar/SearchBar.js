0d3e6de13455828c4aff03ad1cd12ea4828b0e52
cganzon <crganzon@gmail.com>
2022-10-23 17:59:23 -0700

    implement controlled component for the search form

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,7 +1,25 @@
+import { useState } from "react";
 import "./SearchBar.css";
 
 export default function SearchBar() {
+    const [ user, setUser ] = useState({
+        username: ""
+    });
+
+    console.log(user.username)
+    
+    function handleChange(e) {
+        const { name, value } = e.target;
+        setUser(prevUser => ({
+            ...prevUser,
+            [name]: value
+        }));
+    };
+
     return (
-        <h2>Search Bar</h2>
+        <form className="search-form">
+            <input type="text" name="username" value={user.username} onChange={handleChange}/>
+            <button>Search</button>
+        </form>
     );
 };
